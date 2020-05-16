e23d21a36ff6f62ef75637765e6abf4b1cb1f249
Ry Witz <ryanhorowitz@yahoo.com>
2020-05-16 18:59:24 -0400

    random exercise question generated

---
+++ random_number_gen.rb
@@ -0,0 +1,12 @@
+#x = rand(1..11)
+#y = rand(1..10)
+#z = "Do Chapter #{x} Question #{y}!"
+#p z
+
+x = ['variable_scope', 'loops1', 'loops2', 'user_input',
+     'methods', 'return','conditionals', 'string',
+     'arrays', 'hashes', 'debugging' ].sample
+y = rand(1..10)
+z = "Do chapter #{x} question #{y}!"
+
+p z 
